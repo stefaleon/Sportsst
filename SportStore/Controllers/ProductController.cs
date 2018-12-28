645c4a0f545cb2c3499da34b10899ffb8194307d
stefaleon <stefaleon@gmail.com>
2018-12-28 20:52:37 +0200

    ProductController

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List() => View(repository.Products);
+    }
+
+}
