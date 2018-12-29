badbed2450b556cedef79e2801c10451ea03360c
stefaleon <stefaleon@gmail.com>
2018-12-29 13:50:19 +0200

    Disable Scope Verification and Seed Data

--- SportStore/Program.cs
+++ SportStore/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SportStore
 {
@@ -19,6 +12,8 @@ namespace SportStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false);
     }
 }
--- SportStore/Startup.cs
+++ SportStore/Startup.cs
@@ -56,6 +56,7 @@ namespace SportStore
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
