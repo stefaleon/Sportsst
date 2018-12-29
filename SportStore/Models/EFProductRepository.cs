6d87680269503ff14ce2a0e8e1fe0acfcbb53caa
stefaleon <stefaleon@gmail.com>
2018-12-29 12:53:06 +0200

    EFProductRepository

---
+++ SportStore/Models/EFProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace SportStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
